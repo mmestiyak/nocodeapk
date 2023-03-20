1eee23f33b105c1df04c3575f889cb993772c09b
Meer Estiyak <mmestiyak@gmail.com>
2023-03-20 18:50:11 +0600

    added ble

--- App.js
+++ App.js
@@ -1,36 +1,19 @@
+import Bluetooth from './src/components/Bluetooth';
 import React from 'react';
-import {NativeBaseProvider, Box, Button} from 'native-base';
-import useBLE from './src/components/hooks/useBLE';
-import Screen1 from './src/components/Screen1';
 
 const App = () => {
-  const {
-    yourParty,
-    connectedDevice,
-    requestPermissions,
-    scanForPeripherals,
-    connectToDevice,
-    allDevices,
-    billsPC,
-    exchangePokemon,
-  } = useBLE();
-
-  const scanForDevices = () => {
-    requestPermissions(isGranted => {
-      if (isGranted) {
-        scanForPeripherals();
-      }
-    });
-  };
-
   return (
-    <NativeBaseProvider>
-		<Box padding={2}>
-			<Screen1/>
-		</Box>
-	</NativeBaseProvider>
-  )
-
+    <>
+      <Bluetooth />
+    </>
+  );
+  // return (
+  //   <NativeBaseProvider>
+  //     <Box padding={2}>
+  //       <Button onPress={scanForDevices}>hey</Button>
+  //     </Box>
+  //   </NativeBaseProvider>
+  // );
 };
 
 export default App;
---
+++ src/components/hooks/useBLE.js
@@ -0,0 +1,113 @@
+/* eslint-disable no-bitwise */
+import {useState} from 'react';
+import {PermissionsAndroid, Platform} from 'react-native';
+import {BleManager, Device} from 'react-native-ble-plx';
+import DeviceInfo from 'react-native-device-info';
+import {PERMISSIONS, requestMultiple} from 'react-native-permissions';
+
+const bleManager = new BleManager();
+
+function useBLE() {
+  const [allDevices, setAllDevices] = useState([]);
+  const [connectedDevice, setConnectedDevice] = useState();
+
+  const requestPermissions = async cb => {
+    if (Platform.OS === 'android') {
+      const apiLevel = await DeviceInfo.getApiLevel();
+
+      if (apiLevel < 31) {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          {
+            title: 'Location Permission',
+            message: 'Bluetooth Low Energy requires Location',
+            buttonNeutral: 'Ask Later',
+            buttonNegative: 'Cancel',
+            buttonPositive: 'OK',
+          },
+        );
+        cb(granted === PermissionsAndroid.RESULTS.GRANTED);
+      } else {
+        const result = await requestMultiple([
+          PERMISSIONS.ANDROID.BLUETOOTH_SCAN,
+          PERMISSIONS.ANDROID.BLUETOOTH_CONNECT,
+          PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+        ]);
+
+        const isGranted =
+          result['android.permission.BLUETOOTH_CONNECT'] ===
+            PermissionsAndroid.RESULTS.GRANTED &&
+          result['android.permission.BLUETOOTH_SCAN'] ===
+            PermissionsAndroid.RESULTS.GRANTED &&
+          result['android.permission.ACCESS_FINE_LOCATION'] ===
+            PermissionsAndroid.RESULTS.GRANTED;
+
+        cb(isGranted);
+      }
+    } else {
+      cb(true);
+    }
+  };
+
+  const isDuplicateDevice = (devices, nextDevice) =>
+    devices.findIndex(device => nextDevice.localName === device.localName) > -1;
+
+  const scanForPeripherals = () =>
+    bleManager.startDeviceScan(null, null, (error, device) => {
+      if (error) {
+        console.log(error);
+      }
+      if (true) {
+        setAllDevices(prevState => {
+          if (!isDuplicateDevice(prevState, device)) {
+            return [...prevState, device];
+          }
+          return prevState;
+        });
+      }
+    });
+
+  const connectToDevice = async device => {
+    try {
+      const deviceConnection = await bleManager.connectToDevice(device.id);
+      setConnectedDevice(deviceConnection);
+      await deviceConnection.discoverAllServicesAndCharacteristics();
+      bleManager.stopDeviceScan();
+      console.log(device)
+      startStreamingData(deviceConnection);
+    } catch (e) {
+      console.log('FAILED TO CONNECT', e);
+    }
+  };
+
+  const disconnectFromDevice = () => {
+    if (connectedDevice) {
+      bleManager.cancelDeviceConnection(connectedDevice.id);
+      setConnectedDevice(null);
+    }
+  };
+
+  const startStreamingData = async device => {
+    if (device) {
+      console.log(device);
+      // device.monitorCharacteristicForService(
+      //   POKEMON_SERVICE_UUID,
+      //   POKEMON_SERVICE_CHARACTERISTIC,
+      //   onPokemonPartyUpdated,
+      // );
+    } else {
+      console.log('No Device Connected');
+    }
+  };
+
+  return {
+    scanForPeripherals,
+    requestPermissions,
+    connectToDevice,
+    allDevices,
+    connectedDevice,
+    disconnectFromDevice,
+  };
+}
+
+export default useBLE;
